fba4977daba259581fdadd97363aa94198942baa
Mason Walker <masonwalker30@gmail.com>
2022-01-17 22:06:42 -0800

    added timeblocks,  and jumbotron layout

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var date = moment().format('MMMM Do YYYY, h: mm a');
+$("#currentDay").text(date);
+
+var current = moment().hours();
+
+const save = (event) => {
+    if (event.target === button){
+        if(this.classlist.contains(current))
+    }
+}
+
